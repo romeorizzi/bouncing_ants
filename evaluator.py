dbbf5e00b11494752400b2fb52f4bf6debce619d
pietrocipriani <47864521+pietrocipriani@users.noreply.github.com>
2019-05-04 18:29:52 +0200

    aggiornata soluzione ed evaluator

--- evaluator.py
+++ evaluator.py
@@ -3,63 +3,49 @@ import random
 import tempfile 
 import bisect
 
-goals = (("simulate_process_small", 100, 10), ("simulate_process_big", 1000000, 1000))
+goals = (("simulate_process_small", 100, 10), ("simulate_process_big", 100000000, 100))
 
 for goal in goals:
 	W = goal[1]
 	MAX_ANTS = goal[2]
 	
-
-	n_right = 0
-	n_left = 0
-	xs = [[], []]	# [x_left, x_right]
-	for x in range(W//2):
+	ns = [0, 0]			# [n_left, n_right]
+	xs = [[], []]		# [x_left, x_right]
+	names = [[], []]	# [name_left, name_right]
+	merged = []
+	x = random.randint (0, W/MAX_ANTS/2)
+	while ns[0]+ns[1]<MAX_ANTS and x<W:
 		r = random.random()
-		if r > MAX_ANTS*2/W: continue
-		if r < MAX_ANTS/W:
-			xs[0].append(x*2)
-			n_left += 1
-		else:
-			xs[1].append(x*2)
-			n_right += 1
-		if (n_left+n_right >= MAX_ANTS): break
-
-	name_right = [i*2+2 for i in range(n_right)]
-	name_left = [i*2+1 for i in range(n_left)]
+		d = 0 if r<0.5 else 1
+		xs[d].append(x)
+		ns[d] += 1
+		names[d].append(ns[d]*2-1+d)
+		merged.append(ns[d]*2-1+d)
+		x += int(W/MAX_ANTS + (r*2-1)*W/MAX_ANTS/2)
+	
+	solution = dict()		# name: [time, border, guessed]
+	
+	for i in range(ns[1]):
+		solution[merged[-i-1]] = [W-xs[1][-i-1], 1, False]
+	for i in range(ns[0]):
+		solution[merged[i]] = [xs[0][i]+1, 0, False]
 
 	#simulate_process callback
 	def fall_off (time, border, name):
-		d = 1-(name%2)
-		index = [0, 0]
-		index[d] = (name-1)//2
-		index[1-d] = bisect.bisect (xs[1-d], xs[d][index[d]])-1+d
-		if (index[0]<0 or index[0]>len(xs[0])) or (index[1]<0 or index[1]>len(xs[1])):
+		if solution.get(name, None) == None or solution[name][0] != time or solution[name][1] != border:
 			ta.goals.setdefault(goal[0], False)
 			return False
-		
-		overflow = (index[0]+index[1])>(len(xs[0])-1-1+d)
-		
-		if border != overflow:
-			ta.goals.setdefault(goal[0], False)
-			return False
-		if overflow == 0:
-			i = index[0]+index[1]+1-d
-			if time != xs[0][i]+1:
-				ta.goals.setdefault(goal[0], False)
-				return False
-		else:
-			i = index[0]+index[1]-len(xs[0])+1-d
-			if time != W-xs[1][i]:
-				ta.goals.setdefault(goal[0], False)
-				return False
+		solution[name][2] = True
 		return True
-		
-	#for name in name_right: fall_off (0, 0, name)
-	#for name in name_left: fall_off (0, 0, name)
 
 	try:
 		with ta.run_algorithm(ta.submission.source) as process:
-			process.procedures.simulate_process(W,n_right,n_left,xs[1],xs[0],name_right,name_left,callbacks=[fall_off])
+			process.procedures.simulate_process(W,ns[1],ns[0],xs[1],xs[0],names[1],names[0],callbacks=[fall_off])
+			for key, value in solution.items():
+				if not value[2]:
+					ta.goals.setdefault(goal[0], False)
+					break
+		print (f"time usage: {process.time_usage*10**9}")
 	except ta.AlgorithmError as e:
 		ta.goals[goal[0]] = False
 		print(e)
--- solutions/correct.py
+++ solutions/correct.py
@@ -1,23 +1,23 @@
-import bisect
-
-def calc_fall (name, xs, W, report):
-	d = 1-(name%2)
-	index = [0, 0]
-	index[d] = (name-1)//2
-	index[1-d] = bisect.bisect (xs[1-d], xs[d][index[d]])-1+d
+name_merged = []
+		
+def merge (x_right, x_left, name_right, name_left):
+	x1 = 0
+	x2 = 0
+	for _ in range(len(x_right)+len(x_left)):
+		if x2>=len(x_right) or (x1<len(x_left) and x_left[x1] < x_right[x2]):
+			name_merged.append(name_left[x1])
+			x1 += 1
+		else:
+			name_merged.append(name_right[x2])
+			x2 += 1
+		
+		
 	
-	overflow = (index[d]+index[1-d])>(len(xs[0])-1-1+d)
-	
-	if overflow == 0:
-		i = index[d]+index[1-d]+1-d
-		return report (xs[0][i]+1, int(overflow), name)
-	else:
-		i = index[0]+index[1]-len(xs[0])+1-d
-		return report (W-xs[1][i], int(overflow), name)
 
 def simulate_process (W, n_right, n_left, x_right, x_left, name_right, name_left, report_a_fall_off):
-	for i in range(n_left):
-		if not calc_fall (name_left[i], (x_left, x_right), W, report_a_fall_off): return
+	merge (x_right, x_left, name_right, name_left)
+	
 	for i in range(n_right):
-		if not calc_fall (name_right[i], (x_left, x_right), W, report_a_fall_off): return
-		
+		if not report_a_fall_off (W-x_right[-i-1], 1, name_merged[-i-1]): return
+	for i in range(n_left):
+		if not report_a_fall_off (x_left[i]+1, 0, name_merged[i]): return
